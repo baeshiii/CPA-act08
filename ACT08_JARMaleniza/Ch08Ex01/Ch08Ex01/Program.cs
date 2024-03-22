05607f946a4765df1f9003d053ad437db3ab3906
Eunchaechaechae <144511933+baeshiii@users.noreply.github.com>
2024-03-22 18:27:30 +0800

    Add files via upload

---
+++ ACT08_JARMaleniza/Ch08Ex01/Ch08Ex01/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch08Ex01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cnt = 0;
+            Write("Please enter desired number to accept names : ");
+            cnt = Convert.ToInt16(Console.ReadLine());
+
+            string[] lstnames = new string[cnt];
+
+            WriteLine($"The size of my Array is : {lstnames.Length}");
+            WriteLine("Please press any key to continue....");
+            Console.ReadKey();
+
+            WriteLine("Please enter the names now...");
+            for (int i = 0; i < cnt; ++i)
+            {
+                Write($"Array[{i}] <= ");
+                lstnames[i] = Console.ReadLine();
+            }
+
+            WriteLine("\nWe will now display all the names...");
+            Console.ReadKey();
+
+            for (int i = 0; i < cnt; i++)
+            {
+                Write($"Array[{i}] => ");
+                WriteLine(lstnames[i]);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ACT08_JARMaleniza/Ch08Ex02/Ch08Ex02/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+using static System.Convert;
+
+namespace Ch08Ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] friendNames = { "Todd Anthony", "Kevin Holton", "Shane Laigle", null, "" };
+            foreach (var friendName in friendNames)
+            {
+                switch (friendName)
+                {
+                    case string t when t.StartsWith("T"):
+                        WriteLine($"This friend's name starts with a 'T': {friendName} and is {t.Length - 1} letters long ");
+                        break;
+                    case string e when e.Length == 0:
+                        WriteLine("There is a string in the array with no value");
+                        break;
+                    case null:
+                        WriteLine("There was a 'null' value in the array  (although arrays cannot hold null values)");
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            WriteLine();
+            WriteLine("=================================================");
+            WriteLine();
+
+            int sum = 0, total = 0, counter = 0;
+            int[] myIntArray = new int[7] { 5, 0, 9, 10, 0, 2, 99 };
+            foreach (var integer in myIntArray)
+            {
+                switch (integer)
+                {
+                    case 0:
+                        WriteLine($"Integer number '{total}' has a default value of 0");
+                        total++;
+                        break;
+                    case int value:
+                        sum += value;
+                        WriteLine($"Integer number '{total}' has a value of {value}");
+                        total++;
+                        counter++;
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+            WriteLine($"{total} total integers, {counter} integers with a value other than 0 have a sum value of {sum}");
+            ReadLine();
+        }
+
+    }
+}
